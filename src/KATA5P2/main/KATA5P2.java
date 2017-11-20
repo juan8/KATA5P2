3d00366eb5e983c94d5af7008cd2787e6ce0f2c8
PC <PC@PC-PC>
2017-11-20 18:19:23 +0000

    KATA5P2 - versión 2

--- src/KATA5P2/main/KATA5P2.java
+++ src/KATA5P2/main/KATA5P2.java
@@ -6,11 +6,13 @@ import KATA5P2.model.Histogram;
 import KATA5P2.model.Mail;
 import KATA5P2.view.HistogramDisplay;
 import KATA5P2.view.MailHistogramBuilder;
-import KATA5P2.view.MailListReader;
+import KATA5P2.view.MailListReaderDDBB;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
 
 public class KATA5P2 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, FileNotFoundException, ClassNotFoundException, SQLException {
         KATA5P2 kata5 = new KATA5P2();
         kata5.execute();
     }
@@ -18,16 +20,15 @@ public class KATA5P2 {
     private Histogram<String> histogram;
     private List<Mail> mailList;
     
-    private void execute() throws IOException {
+    private void execute() throws IOException, FileNotFoundException, ClassNotFoundException, SQLException {
         input();
         process();
         output();
     }
     
     
-    private void input() throws IOException {
-        String fileName="emails.txt";
-        mailList = MailListReader.read(fileName);
+    private void input() throws IOException, FileNotFoundException, ClassNotFoundException, SQLException {
+        mailList = MailListReaderDDBB.read();
     }
     
     
